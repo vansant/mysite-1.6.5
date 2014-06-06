174bfab72ad6112266943f9f93e1b6ab3c35592e
vansant <dev@Donovans-Mac-mini.local>
2014-06-06 16:55:29 -0700

    Update
    Added TEMPLATE_DIRS to settings.py, registered Poll, Choice models in
    admin.py for use with ‘/admin/’, and updated admin templates

--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,10 @@ class Poll(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published within last 24 hours?'
+
 
 
 class Choice(models.Model):
